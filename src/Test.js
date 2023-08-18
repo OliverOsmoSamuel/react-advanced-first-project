f1868919314a0718a075f9a66f163f681d2183fc
Oliver Janhunen <oliver.janhunen@gmail.com>
2023-08-18 12:06:50 +0300

    first commit

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import Button from "react-bootstrap/Button";
+
+const Test = () => {
+  return (
+    <div>
+      Test will be here
+      <Button variant="primary">Test</Button>
+    </div>
+  );
+};
+
+export default Test;
